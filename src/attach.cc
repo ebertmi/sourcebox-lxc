97fefcd6bd2f650bb321fa4e45d301f6da1a57b9
Johannes Henninger <johannes@henninger.io>
2015-04-09 13:43:37 +0200

    Fix file descriptor flags and file status flags bug

--- src/attach.cc
+++ src/attach.cc
@@ -6,9 +6,20 @@
 
 using namespace v8;
 
-static inline void SetFdFlags(int fd, int flags) {
+static inline int SetFdFlags(int fd, int flags) {
     int oldFlags = fcntl(fd, F_GETFD);
-    fcntl(fd, F_SETFD, oldFlags | flags);
+    if (oldFlags == -1) {
+        return -1;
+    }
+    return fcntl(fd, F_SETFD, oldFlags | flags);
+}
+
+static inline int SetFlFlags(int fd, int flags) {
+    int oldFlags = fcntl(fd, F_GETFL);
+    if (oldFlags == -1) {
+        return -1;
+    }
+    return fcntl(fd, F_SETFL, oldFlags | flags);
 }
 
 bool AttachWorker::inAttachedProcess = false;
@@ -52,14 +63,16 @@ AttachWorker::AttachWorker(lxc_container *container, NanCallback *callback,
         uv_loop_t *loop = uv_default_loop();
 
         // Acquire read lock to prevent the file descriptor from leaking to
-        // other processes before the CLOEXEC flag is set.
+        // other processes before the FD_CLOEXEC flag is set.
         uv_rwlock_rdlock(&loop->cloexec_lock);
 
         openpty(&master, &slave, nullptr, nullptr, nullptr);
-        SetFdFlags(master, FD_CLOEXEC | O_NONBLOCK);
+        SetFdFlags(master, FD_CLOEXEC);
         SetFdFlags(slave, FD_CLOEXEC);
 
         uv_rwlock_rdunlock(&loop->cloexec_lock);
+
+        SetFlFlags(master, O_NONBLOCK);
 
         for (/* reusing fdPos */; fdPos < 3; fdPos++) {
             parentFds[fdPos] = master;
@@ -70,7 +83,7 @@ AttachWorker::AttachWorker(lxc_container *container, NanCallback *callback,
     for (/* reusing fdPos */; fdPos < fdCount; fdPos++) {
         int fds[2];
         socketpair(AF_UNIX, SOCK_STREAM | SOCK_CLOEXEC, 0, fds);
-        SetFdFlags(fds[0], O_NONBLOCK);
+        SetFlFlags(fds[0], O_NONBLOCK);
         parentFds[fdPos] = fds[0];
         childFds[fdPos] = fds[1];
     }
